9dc44331efa90d879e77378f79d72d6dca782a1b
tbitsnitin <nitin.g@tbitsglobal.com>
2013-07-24 11:06:58 +0530

    Package example

---
+++ TestCodes/PackageExample/src/packageExample/Hello.java
@@ -0,0 +1,12 @@
+package packageExample;
+
+public class Hello {
+
+	 
+	
+	public static void main (String st[])
+	{
+	
+		Package.Display();
+	}
+}
---
+++ TestCodes/PackageExample/src/packageExample/Package.java
@@ -0,0 +1,9 @@
+package packageExample;
+
+public class Package
+{
+	public static void Display ()
+	{
+		System.out.println("Hello there");
+	}
+}
